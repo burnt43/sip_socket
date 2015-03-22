c3de2f5f78291a78e84a7ca1f32ea2b6cfa88ca7
James Carson <jcarson@monmouth.com>
2015-03-22 11:35:58 -0400

    made this make more sense

--- foobar.js
+++ foobar.js
@@ -1,5 +1,21 @@
-var sip_client = require('./index.js');
-sip_client.create("200.255.100.116",5060);
-sip_client.on('ready', function () {
-  sip_client.ping_server();
+var sip_socket = require('./index.js');
+sip_socket.create("200.255.100.116",5060);
+sip_socket.on('ready', function () {
+  var message = 'OPTIONS sip:' + sip_socket.destination_host + ' SIP/2.0\n' +
+    'Via: SIP/2.0/UDP ' + sip_socket.source_host + ':' + sip_socket.source_port + ';branch=z9hG4bK26534f84\n' +
+    'Max-Forwards: 70\n' +
+    'From: "unknown" <sip:unknown@' + sip_socket.source_host + ':' + sip_socket.source_port + '>;tag=ba456efda3\n' +
+    'To: <sip:' + sip_socket.destination_host + '>\n' +
+    'Contact: <sip:unknown@' + sip_socket.source_host + ':' + sip_socket.source_port + '>\n' +
+    'Call-ID: 52094582094832@' + sip_socket.source_host + ':' + sip_socket.source_port + '\n' +
+    'CSeq: 102 OPTIONS\n' +
+    'User-Agent: James\n' +
+    'Accept: text/plain\n' + 
+    'Content-Length: 0\n';
+
+  sip_socket.on('OK', function(data) {
+    console.log(data);
+  });
+
+  sip_socket.write(message);
 });
--- index.js
+++ index.js
@@ -2,12 +2,18 @@ var dgram = require('dgram');
 var EventEmitter = require('events').EventEmitter;
 var destination_host, destination_port, socket;
 var source_host, source_port;
-var retransmits = 5;
 
 
 function parse_sip_response (response) {
   response_hash = {};
-  sip_header_found = false;
+  
+  function parse_sip_header(line) {
+    if ( line.match(/OK/) ) {
+      return "OK";
+    } else {
+      return "UNKNOWN";
+    }
+  }
 
   function parse_sip_line (line) {
     line = line.substring(0,line.length-1);
@@ -18,9 +24,10 @@ function parse_sip_response (response) {
     if ( name.slice(-1) == ':' ) {
       response_hash[name.substring(0,name.length-1)] = info;
     } else if ( name.length > 0 ) {
+      response_hash['MessageType'] = parse_sip_header(line);
       response_hash['Header'] = line;
     } else {
-      //emit event
+      module.exports.emit(response_hash['MessageType'],response_hash);
     }
   }
 
@@ -31,8 +38,6 @@ function parse_sip_response (response) {
     remaining_string = remaining_string.substring(newline + 1);
     parse_sip_line(current_string);
   }
-  console.log(response_hash);
-  return response_hash;
 }
 
 module.exports = new EventEmitter();
@@ -45,43 +50,24 @@ module.exports.create = function (host,port) {
   socket.bind(null, 'localhost',  function () {
     source_host = socket.address().address;
     source_port = socket.address().port;
+    module.exports.destination_host = destination_host;
+    module.exports.destination_port = destination_port;
+    module.exports.source_host = source_host;
+    module.exports.source_port = source_port;
     module.exports.emit('ready');
   });
 };
 
-function ping_server() {
-  var server_response = null;
-
-  var message = new Buffer('OPTIONS sip:' + destination_host + ' SIP/2.0\n' +
-    'Via: SIP/2.0/UDP ' + source_host + ':' + source_port + ';branch=z9hG4bK26534f84\n' +
-    'Max-Forwards: 70\n' +
-    'From: "unknown" <sip:unknown@' + source_host + ':' + source_port + '>;tag=ba456efda3\n' +
-    'To: <sip:' + destination_host + '>\n' +
-    'Contact: <sip:unknown@' + source_host + ':' + source_port + '>\n' +
-    'Call-ID: 52094582094832@' + source_host + ':' + source_port + '\n' +
-    'CSeq: 102 OPTIONS\n' +
-    'User-Agent: James\n' +
-    'Accept: text/plain\n' + 
-    'Content-Length: 0\n'
-  );
-  
-  function send_ping() {
-    socket.send(message, 0, message.length, destination_port, destination_host, function (err) {});
-    setTimeout( function () {
-      if ( !server_response ) {
-        send_ping();
-      } else {
-      }
-    }, 3000 );
-  }
-
+module.exports.write = function (string) {
+  var message = new Buffer(string);
   socket.on('message', function (data,info) {
-    hash = parse_sip_response(data.toString());
-    server_response = hash['Header'].match(/200 OK/);
+    parse_sip_response(data.toString());
   });
+  socket.send(message, 0, message.length, destination_port, destination_host, function (err) {
+    if ( err ) {
+      console.log(err);
+      socket.close();
+    }
+  });
+}
 
-  send_ping();
-
-};
-
-module.exports.ping_server = ping_server;
