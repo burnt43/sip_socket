04cf709982457623d0c350f1ab56f997b1178616
James Carson <jcarson@monmouth.com>
2015-03-28 01:22:28 -0400

    I can now register with asterisk!

--- foobar.js
+++ foobar.js
@@ -1,4 +1,5 @@
 var sip_socket = require('./index.js');
+var crypto = require('crypto');
 sip_socket.create("200.255.100.116",5060);
 sip_socket.on('ready', function () {
   var options_message = 'OPTIONS sip:' + sip_socket.destination_host + ' SIP/2.0\n' +
@@ -13,34 +14,82 @@ sip_socket.on('ready', function () {
     'Accept: text/plain\n' + 
     'Content-Length: 0\n';
 
-  var sip_user_name = "284-eng";
+  var user = "mrfoobar";
+  var pass = "slapboat";
 
-  var register_message = 'REGISTER sip:' + sip_socket.destination_address + ' SIP/2.0\n' +
-    'Via: SIP/2.0/UDP ' + sip_socket.source_host + ':' + sip_socket.source_port + ';branch=z9hG4bK26534f84\n' +
-    'From: "unknown" <sip:' + sip_user_name + '@' + sip_socket.destination_host + '>;tag=3ceca9d67449aac6o0^\n' +
-    'To: "unknown" <sip:' + sip_user_name + '@' + sip_socket.destination_host + '>\n' +
-    'Call-ID: da611025-55e45e25@' + sip_socket.source_host + '\n' +
-    'CSeq: 60591 REGISTER\n' +
-    'Contact: "unknown" <sip:' + sip_user_name + '@' + sip_socket.source_host + '>;expires=300\n' +
-    'Authorization: Digest username="' + sip_user_name + '", realm="asterisk", nonce="6fe6b930", uri="sip:' + sip_socket.destination_host + '", response="102cdef93746278a24031cffbcf6618b", algorithm=MD5\n' +
-    'Allow: ACK, BYE, CANCEL, INFO, INVITE, NOTIFY, OPTIONS, REFER, UPDATE\n' +
-    'Max-forwards: 69\n' +
-    'User-agent: foobar\n' +
-    'Supported: replaces\n' +
-    'Content-Length: 0\n';
+  function random_number () {
+    return Math.floor(Math.random() * 10000000).toString(10);
+  }
+
+  function md5_hash (input) {
+    console.log(input)
+    return crypto.createHash('md5').update(input).digest('hex') + "";
+  }
+
+  function generate_nonce () {
+    return generate_response().substring(0,8);
+  }
+
+  function generate_response (username,nonce,realm,password) {
+    if ( !username || !nonce || !realm || !password) {
+      return md5_hash( random_number() );
+    } else {
+      return md5_hash( md5_hash(username + ':' + realm + ':' + password) + ':' + nonce + ':' + md5_hash('REGISTER:sip:'+sip_socket.destination_host) );
+    }
+  }
+
+  function create_register_message (s_address, s_port, d_address, d_port, sip_user_name, password, nonce, response) {
+      if ( !nonce ) {nonce = generate_nonce();}
+      if ( !response ) {response = generate_response()}
+
+      return 'REGISTER sip:' + d_address + ' SIP/2.0\n' +
+      'Via: SIP/2.0/UDP ' + s_address + ':' + s_port + ';branch=z9hG4bK26534f84\n' +
+      'From: "unknown" <sip:' + sip_user_name + '@' + d_address + '>;tag=3ceca9d67449aac6o0^\n' +
+      'To: "unknown" <sip:' + sip_user_name + '@' + d_address + '>\n' +
+      'Call-ID: da611025-55e45e25@' + s_address + '\n' +
+      'CSeq: 60591 REGISTER\n' +
+      'Contact: "unknown" <sip:' + sip_user_name + '@' + s_address + '>;expires=300\n' +
+      'Authorization: Digest username="' + sip_user_name + '", realm="asterisk", nonce="' + nonce + '", uri="sip:' + d_address + '", response="' + response + '", algorithm=MD5\n' +
+      'Allow: ACK, BYE, CANCEL, INFO, INVITE, NOTIFY, OPTIONS, REFER, UPDATE\n' +
+      'Max-forwards: 69\n' +
+      'User-agent: foobar\n' +
+      'Supported: replaces\n' +
+      'Content-Length: 0\n';
+  }
 
   sip_socket.on('200', function(data) {
     console.log(data);
   });
 
   sip_socket.on('401', function (data) {
+    console.log('GOT DATA');
+    console.log(data);
+    console.log('RESPONDING');
+    nonce = data['WWW-Authenticate']['nonce'];
+    realm = data['WWW-Authenticate']['realm'];
+    response = generate_response(user,nonce,realm,pass);
+    sip_socket.write( create_register_message(sip_socket.source_host,
+                                              sip_socket.source_port,
+                                              sip_socket.destination_host,
+                                              sip_socket.destination_port,
+                                              user,
+                                              pass,
+                                              nonce,
+                                              response) );
+  });
+
+  sip_socket.on('403', function(data) {
     console.log(data);
   });
 
-  sip_socket.on('481', function(data) {
+  sip_socket.on('482', function(data) {
     console.log("LOOP BS");
   });
 
-  //sip_socket.write(options_message);
-  sip_socket.write(register_message);
+  sip_socket.write( create_register_message(sip_socket.source_host,
+                                            sip_socket.source_port,
+                                            sip_socket.destination_host,
+                                            sip_socket.destination_port,
+                                            user,
+                                            pass) );
 });
