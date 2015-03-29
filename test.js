d4dc466783e1b2cec6cb53aee62c67ec797feee3
James Carson <jcarson@monmouth.com>
2015-03-28 22:17:45 -0400

    rewrote the testing script with new object oriented SipSocket. created SipMessageTemplates.

--- test.js
+++ test.js
@@ -1,22 +1,30 @@
-var sip_socket = require('./index.js');
-var crypto = require('crypto');
+var SipSocket = require('./sip_socket.js');
+var SipMessageTemplates = require('./sip_message_templates.js');
+var sip_socket = new SipSocket ("200.255.100.116","5060");
+sip_socket.connect();
+sip_socket.on('connection_established', function () {
 
-sip_socket.create("200.255.100.116",5060);
+  function replace_values (string,hash) {
 
-sip_socket.on('ready', function () {
+    var replacement_string = string;
+    var regex;
+
+    Object.keys(hash).forEach( function (key) {
+      regex = new RegExp( '\\$' + key.toUpperCase(), 'g' );
+      replacement_string = replacement_string.replace( regex, hash[key] );
+    });
+
+    return replacement_string;
+  }
 
   function ping () {
-    var options_message = 'OPTIONS sip:' + sip_socket.destination_host + ' SIP/2.0\n' +
-      'Via: SIP/2.0/UDP ' + sip_socket.source_host + ':' + sip_socket.source_port + ';branch=z9hG4bK26534f84\n' +
-      'Max-Forwards: 70\n' +
-      'From: "unknown" <sip:unknown@' + sip_socket.source_host + ':' + sip_socket.source_port + '>;tag=ba456efda3\n' +
-      'To: <sip:' + sip_socket.destination_host + '>\n' +
-      'Contact: <sip:unknown@' + sip_socket.source_host + ':' + sip_socket.source_port + '>\n' +
-      'Call-ID: 52094582094832@' + sip_socket.source_host + ':' + sip_socket.source_port + '\n' +
-      'CSeq: 102 OPTIONS\n' +
-      'User-Agent: James\n' +
-      'Accept: text/plain\n' + 
-      'Content-Length: 0\n';
+
+    var options_message = replace_values( SipMessageTemplates.options, {
+      'source_address': sip_socket.get_source_address(),
+      'source_port': sip_socket.get_source_port(),
+      'destination_address': sip_socket.get_sip_server_address(),
+      'destination_port': sip_socket.get_sip_server_port()
+    });
 
     sip_socket.on('200', function (data) {
       console.log(data);
@@ -25,14 +33,15 @@ sip_socket.on('ready', function () {
     sip_socket.write(options_message);
   }
 
+
   function register (user,pass) {
 
     function random_number () {
       return Math.floor(Math.random() * 10000000).toString(10);
     }
 
-    function md5_hash (input) {
-      return crypto.createHash('md5').update(input).digest('hex') + "";
+    function md5_sum (input) {
+      return require('crypto').createHash('md5').update(input).digest('hex') + "";
     }
 
     function generate_nonce () {
@@ -41,31 +50,29 @@ sip_socket.on('ready', function () {
 
     function generate_response (username,nonce,realm,password) {
       if ( !username || !nonce || !realm || !password) {
-        return md5_hash( random_number() );
+        return md5_sum( random_number() );
       } else {
-        return md5_hash( md5_hash(username + ':' + realm + ':' + password) + ':' + nonce + ':' + md5_hash('REGISTER:sip:'+sip_socket.destination_host) );
+        return md5_sum( md5_sum(username + ':' + realm + ':' + password) + ':' + nonce + ':' + md5_sum('REGISTER:sip:'+sip_socket.get_sip_server_address()) );
       }
     }
 
-    var callid = generate_response() + "@" + sip_socket.source_host;
+    var callid = generate_response() + "@" + sip_socket.get_source_address();
 
-    function create_register_message (s_address, s_port, d_address, d_port, sip_user_name, password, nonce, response) {
+    function create_register_message (sip_user_name, password, nonce, response) {
         if ( !nonce ) { nonce = generate_nonce(); }
         if ( !response ) { response = generate_response() }
 
-        return 'REGISTER sip:' + d_address + ' SIP/2.0\n' +
-        'Via: SIP/2.0/UDP ' + s_address + ':' + s_port + ';branch=z9hG4bK26534f84\n' +
-        'From: "unknown" <sip:' + sip_user_name + '@' + d_address + '>;tag=3ceca9d67449aac6o0\n' +
-        'To: "unknown" <sip:' + sip_user_name + '@' + d_address + '>\n' +
-        'Call-ID: ' + callid + '\n' +
-        'CSeq: 60591 REGISTER\n' +
-        'Contact: "unknown" <sip:' + sip_user_name + '@' + s_address + '>;expires=300\n' +
-        'Authorization: Digest username="' + sip_user_name + '", realm="asterisk", nonce="' + nonce + '", uri="sip:' + d_address + '", response="' + response + '", algorithm=MD5\n' +
-        'Allow: ACK, BYE, CANCEL, INFO, INVITE, NOTIFY, OPTIONS, REFER, UPDATE\n' +
-        'Max-forwards: 69\n' +
-        'User-agent: foobar\n' +
-        'Supported: replaces\n' +
-        'Content-Length: 0\n';
+
+        return replace_values( SipMessageTemplates.register, {
+          'source_address': sip_socket.get_source_address(),
+          'source_port': sip_socket.get_source_port(),
+          'destination_address': sip_socket.get_sip_server_address(),
+          'destination_port': sip_socket.get_sip_server_port(),
+          'sip_user_name': user,
+          'sip_nonce': nonce,
+          'sip_response': response,
+          'sip_callid': callid
+        });
     }
 
     sip_socket.on('401', function (data) {
@@ -73,9 +80,7 @@ sip_socket.on('ready', function () {
       nonce = data['WWW-Authenticate']['nonce'];
       realm = data['WWW-Authenticate']['realm'];
       response = generate_response(user,nonce,realm,pass);
-      sip_socket.write( create_register_message(sip_socket.source_host,sip_socket.source_port,
-                                                sip_socket.destination_host,sip_socket.destination_port,
-                                                user,pass,nonce,response) );
+      sip_socket.write( create_register_message(user,pass,nonce,response) );
     });
 
     sip_socket.on('200', function(data) {
@@ -84,21 +89,15 @@ sip_socket.on('ready', function () {
 
     sip_socket.on('403', function(data) {
       console.log(data);
-      setTimeout( function () {
-        sip_socket.clear();
-        register(user,pass);
-      }, 5000);
     });
 
     sip_socket.on('482', function(data) {
       console.log(data);
     });
 
-    sip_socket.write( create_register_message(sip_socket.source_host,sip_socket.source_port,
-                                              sip_socket.destination_host,sip_socket.destination_port,
-                                              user,pass) );
+    sip_socket.write( create_register_message(user,pass) );
 
   }
 
-  register("mrfoobar","slapboat");
+  register('mrfoobar','slapboat');
 });
