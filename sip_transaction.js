9a60b21a9814de70bff099b295329b1d81565e0b
James Carson <jcarson@monmouth.com>
2015-03-29 01:39:26 -0400

    removed these useless files

--- sip_transaction.js
+++
@@ -1,5 +0,0 @@
-function SipTransaction (sip_transaction_manager) {
-  this.sip_transaction_manager = sip_transaction_manager;
-}
-
-module.exports = SipTransaction;
--- sip_transaction_manager.js
+++
@@ -1,4 +0,0 @@
-function SipTransactionManager () {
-}
-
-module.exports = SipTransactionManager;
