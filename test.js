3abf0c75b18a6e458e683f526b8e4219500ce91c
James Carson <jcarson@monmouth.com>
2015-03-29 01:43:17 -0400

    moved client stuff to its own repo

--- test.js
+++
@@ -1,7 +0,0 @@
-var SipClient = require('./sip_client.js');
-var sip_client = new SipClient("200.255.100.116","5060","mrfoobar","slapboat");
-
-sip_client.connect();
-sip_client.on('connection_established', function () {
-  sip_client.register();
-});
