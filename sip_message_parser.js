78da3b11d610e0a819f70c005b75c44ffc99b664
James Carson <jcarson@monmouth.com>
2015-05-01 23:19:47 -0400

    the parse can now parse sip messages with Content-Length > 0

--- sip_message_parser.js
+++ sip_message_parser.js
@@ -1,24 +1,29 @@
 module.exports.parse = function (response) {
   
+  //console.log('------------------------------------------');
+  //console.log(response);
+
   function parse_sip_header(line) {
     if ( result = line.match(/^SIP\/2.0\s([0-9]+)/) ) {
       return result[1];
     } else {
-      return "UNKNOWN";
+      return 'UNKNOWN';
     }
   }
 
   function parse_info (hash_key,info) {
     switch (hash_key) {
-      case "WWW-Authenticate":
+      case 'Content-Length':
+        return parseInt(info);
+      case 'WWW-Authenticate':
         var hash = {};
         info.split(',').forEach( function (key_value_pair) {
           var split_pair = key_value_pair.trim().split('=');
-          hash[split_pair[0]] = split_pair[1].replace(/['"]/g,"");
+          hash[split_pair[0]] = split_pair[1].replace(/['"]/g,'');
         });
         return hash;
         break;
-      case "Allow":
+      case 'Allow':
         return info.split(',').map( function (allow_string) {
           return allow_string.trim();
         });
@@ -42,11 +47,27 @@ module.exports.parse = function (response) {
     }
   }
 
-  var response_by_line = response.split('\n');
-  var response_hash = {};
-  response_by_line.forEach( function (line) {
-    parse_sip_line(line.trim());
-  });
+  function parse_content ( content_string ) {
+    return {};
+  }
+
+  var response_hash    = {};
+  var remaining_string = response;
+
+  while ( remaining_string.length > 0 ) {
+    var index_of_newline = remaining_string.indexOf('\n');
+    var current_line     = remaining_string.substring(0,index_of_newline);
+    var remaining_string = remaining_string.substring(index_of_newline+1);
+
+    //console.log('current_line(' + current_line.length + ',' + current_line.charCodeAt(0) + ') ' + current_line);
+    if ( current_line == '\r' ) {
+      response_hash['Content'] = parse_content(remaining_string);
+      break;
+    } else {
+      parse_sip_line(current_line.trim());
+    }
+
+  }
 
   return response_hash;
 }
